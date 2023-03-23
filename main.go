f0de910f81c2e05641bc392f768ab20deec86357
lper <lper@foxmail.com>
2023-03-24 01:32:03 +0800

    更新~

--- main.go
+++ main.go
@@ -71,9 +71,7 @@ func proxy(c *gin.Context) {
 	c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 	c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
 	c.Header("Access-Control-Allow-Credentials", "true")
-	if method == "OPTIONS" {
-		c.AbortWithStatus(http.StatusNoContent)
-	}
+	
 	// Get status code
 	c.Status(response.StatusCode)
 	c.Stream(func(w io.Writer) bool {
